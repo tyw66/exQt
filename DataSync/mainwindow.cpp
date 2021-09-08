2fd161ac0782c9cf428dda38c84b0d5b66b6d541
tyw66 <2639144635@qq.com>
2021-09-09 00:22:09 +0800

    [数据同步]添加组件完成OK

--- DataSync/mainwindow.cpp
+++ DataSync/mainwindow.cpp
@@ -10,12 +10,14 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-
     initConnectA();
     initConnectB();
     initConnectC();
     initConnectD();
+    initConnectE();
+    initConnectF();
 
+    initConnect1();
 }
 
 MainWindow::~MainWindow()
@@ -72,7 +74,6 @@ void MainWindow::onReactB(const QString& msg)
     ui->listWidgetB->setCurrentRow(-1);//取消选择
     if(!list.isEmpty()){
         ui->listWidgetB->setCurrentItem(list.first());
-
     }
     ui->listWidgetB->blockSignals(blocked);
 }
@@ -140,4 +141,87 @@ void MainWindow::onReactD(const QString &msg)
 
     ui->spinBox->blockSignals(blocked);
 }
+
+
+//--组件E--
+void MainWindow::initConnectE()
+{
+    //发
+    connect(ui->comboBox,SIGNAL(currentTextChanged(QString)),this,SLOT(onChangeComboBox(QString)));
+    //收
+    connect(SignalHub::inst(),SIGNAL(state1Changed(QString)),this,SLOT(onReactE(QString)));
+}
+
+void MainWindow::onChangeComboBox(const QString &value)
+{
+    qDebug() << __FUNCTION__;
+    SignalHub::inst()->emitState1Changed(value);
+}
+
+void MainWindow::onReactE(const QString &msg)
+{
+    qDebug() << __FUNCTION__;
+    bool blocked = ui->comboBox->blockSignals(true);
+
+    ui->comboBox->setCurrentText(msg);
+
+    ui->comboBox->blockSignals(blocked);
+}
+
+//--组件F--
+void MainWindow::initConnectF()
+{
+    //收
+    connect(SignalHub::inst(),SIGNAL(state1Changed(QString)),this,SLOT(onReactF(QString)));
+}
+
+void MainWindow::onReactF(const QString &msg)
+{
+    qDebug() << __FUNCTION__;
+    bool blocked = ui->toolBox->blockSignals(true);
+
+    int index = msg.toInt();
+    ui->toolBox->setCurrentIndex(index);
+
+    ui->toolBox->blockSignals(blocked);
+}
+
+
+//--被动组件1--
+void MainWindow::initConnect1()
+{
+    //收
+    connect(SignalHub::inst(),SIGNAL(state1Changed(QString)),this,SLOT(onReact1(QString)));
+}
+
+void MainWindow::onReact1(const QString &msg)
+{
+    qDebug() << __FUNCTION__;
+    bool blocked_ji = ui->radioButton_ji->blockSignals(true);
+    bool blocked_ou = ui->radioButton_ou->blockSignals(true);
+
+    bool ok;
+    int value = msg.toInt(&ok, 10);
+    if(ok && ok==true){
+        if(value%2==0){
+            ui->radioButton_ou->setChecked(true);
+        }
+        else{
+            ui->radioButton_ji->setChecked(true);
+        }
+    }else{
+        //取消两个radioButton的选中状态
+        ui->radioButton_ji->setAutoExclusive(false);
+        ui->radioButton_ou->setAutoExclusive(false);
+        ui->radioButton_ji->setChecked(false);
+        ui->radioButton_ou->setChecked(false);
+        ui->radioButton_ji->setAutoExclusive(true);
+        ui->radioButton_ou->setAutoExclusive(true);
+    }
+
+    ui->radioButton_ji->blockSignals(blocked_ji);
+    ui->radioButton_ou->blockSignals(blocked_ou);
+}
+
+
 
--- DataSync/mainwindow.h
+++ DataSync/mainwindow.h
@@ -19,34 +19,54 @@ private:
     Ui::MainWindow *ui;
 
 
-    //----A------------------------
+    //----组件A------------------------
 private:
     void initConnectA();    //连接组件A的信号槽
 private slots:
     void onClickButtonA();  //调用公用模块发信号
     void onReactA(const QString &msg);  //槽函数响应
 
-    //----B------------------------
+    //----组件B------------------------
 private:
     void initConnectB();
 private slots:
     void onClickButtonB();
     void onReactB(const QString &msg);
 
-    //----C------------------------
+    //----组件C------------------------
 private:
     void initConnectC();
 private slots:
     void onChangeListWidget();
     void onReactC(const QString &msg);
 
-
-    //----D------------------------
+    //----组件D------------------------
 private:
     void initConnectD();
 private slots:
     void onChangeSpinBox(const QString& value);
     void onReactD(const QString &msg);
+
+    //----组件E------------------------
+private:
+    void initConnectE();
+private slots:
+    void onChangeComboBox(const QString& value);
+    void onReactE(const QString &msg);
+
+    //----组件E------------------------
+private:
+    void initConnectF();
+private slots:
+    void onReactF(const QString &msg);
+
+
+    //----被动组件1------------------------
+private:
+    void initConnect1();
+private slots:
+    void onReact1(const QString &msg);
+
 
 
 };
