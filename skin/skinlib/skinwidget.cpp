008e353ccc5315c68197838495a16ddeba0d4ebc
tyw66 <2639144635@qq.com>
2021-11-04 01:10:28 +0800

    皮肤切换：修改为重启才能生效，并且切换图标。存在问题：qrc文件不能加在工程中

--- skin/skinlib/skinwidget.cpp
+++ skin/skinlib/skinwidget.cpp
@@ -10,14 +10,21 @@
 #define RCC_DIR_NAME "rcc"
 //qrc资源文件里用于存放QSS的前缀
 #define QRC_QSS_PREFIX "stylesheet"
-
+//配置文件中皮肤的关键字
+#define INI_KEY_SKIN "skin"
+//配置文件路径
+#define INIFILE_PATH "./settings.ini"
 
 SkinWidget::SkinWidget(QWidget *parent) : QWidget(parent)
 {
-    initUI();
-    initSkin();
-}
+    m_settings = new QSettings(INIFILE_PATH,QSettings::IniFormat);
 
+    initUI();
+    initSkinItem();
+
+    initConnect();
+    readSkin();
+}
 
 void SkinWidget::initUI()
 {
@@ -29,76 +36,97 @@ void SkinWidget::initUI()
 
     m_comboBox = new QComboBox(this);
     hLayout->addWidget(m_comboBox);
-    connect(m_comboBox,SIGNAL(currentIndexChanged(int)),
-            this, SLOT(onSwitch(int)));
-
 }
 
-void SkinWidget::initSkin()
+void SkinWidget::initConnect()
 {
-    //添加1个空的
-    m_qssString.push_back("");
-    m_comboBox->addItem("No Skin (native)", "");
+    connect(m_comboBox,SIGNAL(currentIndexChanged(int)),
+            this, SLOT(onSwitch(int)));
+}
 
-    //遍历文件夹的rcc，添加内置的
+void SkinWidget::initSkinItem()
+{
+    //添加1个空的皮肤
+    QString skinNameNative = "(native)";
+    m_comboBox->addItem(skinNameNative, "");
+
+    //遍历文件夹的rcc，添加内置的皮肤
     QString rccPath = QApplication::applicationDirPath()+"/" + RCC_DIR_NAME + "/";
     QFileInfoList rccFiles = QDir(rccPath).entryInfoList(QStringList("*.rcc"),
-                                                      QDir::Files | QDir::Readable, QDir::Name);
-
+                                                         QDir::Files | QDir::Readable, QDir::Name);
 
     for (int i = 0; i < rccFiles.length(); ++i) {
         QFileInfo info = rccFiles[i];
-        QString rccFilePath = info.absoluteFilePath();
+
+        //m_comboBox添加
         QString skinName = info.baseName();
-        QString skinFilePath = QString(":/%1/%2.qss")
-                .arg(QRC_QSS_PREFIX)
-                .arg(skinName);
-
-        qDebug() << "Load skin:" << skinName;
-        //切换rcc资源
-        QResource::registerResource(rccFilePath);
-        //
-        appendSkin(skinName,skinFilePath,rccFilePath);
-
+        QString rccFilePath = info.absoluteFilePath();
+        m_comboBox->addItem(skinName, rccFilePath);
     }
+
+    //根据配置文件，添加扩展配置的皮肤TODO
+
 }
 
-void SkinWidget::onSwitch(int index)
+QString SkinWidget::readStyleSheet(const QString &qssFilePath)
 {
-    //切换rcc资源
-    QString rccFile = m_comboBox->currentData().toString();
-    QResource::registerResource(rccFile);
-
-
-    //切换QSS
-    assert(index < m_qssString.size());
-    QString qssContent = m_qssString.at(index);
-    qApp->setStyleSheet(qssContent);
-}
-
-
-
-void SkinWidget::appendSkin(const QString &skinName,
-                            const QString &skinFilePath,
-                            const QString &rccFilePath)
-{
-    //读取qss内容
     QString stylesheet;
-    QFile qss(skinFilePath);
-    if (!QFile::exists(skinFilePath)) {
-        qDebug() << tr("Can not find skin file. (%1)").arg(skinFilePath);
-        return;
+    QFile qss(qssFilePath);
+    if (!QFile::exists(qssFilePath)) {
+        qDebug() << tr("Can not find skin file. (%1)").arg(qssFilePath);
+        return stylesheet;
     }
 
     if (qss.open(QFile::ReadOnly)) {
         stylesheet = tr(qss.readAll());
     } else {
-        qDebug() << tr("Failed to read skin file. (%1)").arg(skinFilePath);
-        return;
+        qDebug() << tr("Failed to read skin file. (%1)").arg(qssFilePath);
+        return stylesheet;
     }
     qss.close();
 
-    //添加
-    m_qssString.push_back(stylesheet);
-    m_comboBox->addItem(skinName, rccFilePath);
+    return stylesheet;
+}
+
+
+void SkinWidget::onSwitch(int index)
+{
+    //更新配置文件
+    writeSkin();
+
+    //向外部发信号
+    emit skinChanged();
+}
+
+void SkinWidget::readSkin()
+{
+    //读取配置文件中记录的当前皮肤，并切换，如果不存在，切换为第一个。
+    if(m_settings){
+        QString skinName = m_settings->value(INI_KEY_SKIN).toString();
+        int index = m_comboBox->findText(skinName);
+        m_comboBox->setCurrentIndex(index == -1 ? 0 : index);
+    }
+    else{
+        m_comboBox->setCurrentIndex(0);
+    }
+
+    //启动时动态加载RCC资源[只执行1次]
+    QString rccFile = m_comboBox->currentData().toString();
+    QResource::registerResource(rccFile);
+
+    //加载QSS
+    QString skinName = m_comboBox->currentText();
+    QString skinFilePath = QString(":/%1/%2.qss")
+            .arg(QRC_QSS_PREFIX).arg(skinName);
+
+    qApp->setStyleSheet(readStyleSheet(skinFilePath));
+}
+
+void SkinWidget::writeSkin()
+{
+    //向配置文件中更新当前皮肤的记录
+    if(m_settings){
+        QString skinName = m_comboBox->currentText();
+        m_settings->setValue(INI_KEY_SKIN, skinName);
+    }
 }
--- skin/skinlib/skinwidget.h
+++ skin/skinlib/skinwidget.h
@@ -4,8 +4,9 @@
 #include <QWidget>
 #include <QComboBox>
 #include <QLabel>
-#include "skinwidget_global.h"
+#include <QSettings>
 
+#include "skinwidget_global.h"
 
 class SKINWIDGETSHARED_EXPORT SkinWidget : public QWidget
 {
@@ -16,25 +17,34 @@ public:
 
 private slots:
     //切换皮肤响应
-    void onSwitch(int index);
+    void onSwitch(int);
 
 
 private:
-    void initUI();
-    void initSkin();
+    ///初始化UI
+    inline void initUI();
+    ///初始化信号槽
+    inline void initConnect();
+    ///初始化皮肤
+    void initSkinItem();
 
+    ///读取qss内容
+    QString readStyleSheet(const QString& skinFilePath);
 
-    void appendSkin(const QString& skinName,
-                    const QString& skinFilePath,
-                    const QString& rccFilePath);
+    ///读取配置文件中的当前皮肤 位于"skin"关键字
+    void readSkin();
+    /// 向配置文件中的当前皮肤 位于"skin"关键字
+    void writeSkin();
 
+signals:
+    void skinChanged();
 
 private:
     QLabel* m_label;
     QComboBox* m_comboBox;
 
-    QList<QString> m_qssString;
 
+    QSettings* m_settings;
 };
 
 #endif // SKINWIDGET_H
